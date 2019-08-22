8fc227f23e592d52a5b309a25c3272eeb91b34ed
Marius Vigariu <marius.vigariu@onem.com>
2019-08-22 14:58:27 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_descr = f.read()
+
+
+setuptools.setup(
+    name='onem-py',
+    version='0.0.1',
+    author='Marius Vigariu',
+    author_email='marius.vigariu@onem.com',
+    description='Defines the JSON structure accepted by ONEm platform.',
+    long_description=long_descr,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mvnm/onem-py',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
